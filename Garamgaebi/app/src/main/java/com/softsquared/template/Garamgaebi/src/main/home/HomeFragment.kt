8136b8bd4f9730b72faf5cbf70d03d40576744ad
Dongwon Shin <edvedv@naver.com>
2023-01-15 05:26:59 +0900

    Feat: home screen implementation

--- Garamgaebi/app/src/main/java/com/softsquared/template/Garamgaebi/src/main/home/HomeFragment.kt
+++ Garamgaebi/app/src/main/java/com/softsquared/template/Garamgaebi/src/main/home/HomeFragment.kt
@@ -5,10 +5,15 @@ import android.view.View
 import com.softsquared.template.Garamgaebi.R
 import com.softsquared.template.Garamgaebi.config.BaseFragment
 import com.softsquared.template.Garamgaebi.databinding.FragmentHomeBinding
+import com.softsquared.template.Garamgaebi.src.main.networking.NetworkingProfile
 
 
 class HomeFragment : BaseFragment<FragmentHomeBinding>(FragmentHomeBinding::bind, R.layout.fragment_home) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        var seminarDataList: ArrayList<HomeSeminarItemData> = arrayListOf(
+            HomeSeminarItemData(true, "로건 세미나", "xxxx-xx-xx", "pp", 1),
+            HomeSeminarItemData(false, "신디 세미나", "xxxx-xx-xx", "pp", 2)
+        )
     }
 }
---
+++ Garamgaebi/app/src/main/java/com/softsquared/template/Garamgaebi/src/main/home/HomeSeminarItemData.kt
@@ -0,0 +1,9 @@
+package com.softsquared.template.Garamgaebi.src.main.home
+
+data class HomeSeminarItemData(
+    val pay : Boolean,
+    val name : String,
+    val date : String,
+    val place : String,
+    val dDay : Int,
+)
