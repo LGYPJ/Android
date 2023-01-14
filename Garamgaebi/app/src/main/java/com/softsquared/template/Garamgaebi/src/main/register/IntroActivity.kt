5c3d41fabf38d52a88fb6a9df2a6a7dcbb6d1588
Dongwon Shin <edvedv@naver.com>
2023-01-15 01:26:58 +0900

    Revert "세미나 신청 merge 수정"
    This reverts commit 8f69fdb483f84d724bb0580a360d721ed0afa25f.
    
    # Conflicts:
    #	Garamgaebi/app/src/main/java/com/softsquared/template/Garamgaebi/src/main/home/HomeFragment.kt

---
+++ Garamgaebi/app/src/main/java/com/softsquared/template/Garamgaebi/src/main/register/IntroActivity.kt
@@ -0,0 +1,63 @@
+package com.softsquared.template.Garamgaebi.src.main.register
+
+import android.content.Intent
+import android.os.Bundle
+import android.util.Log
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import androidx.viewpager2.widget.ViewPager2
+import com.softsquared.template.Garamgaebi.config.BaseActivity
+import com.softsquared.template.Garamgaebi.databinding.ActivityIntroBinding
+import com.softsquared.template.Garamgaebi.src.main.MainActivity
+
+class IntroActivity : BaseActivity<ActivityIntroBinding>(ActivityIntroBinding::inflate) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val vpAdapter = IntroViewPagerAdapter(this)
+        binding.activityIntroVp.adapter = vpAdapter
+
+        binding.activityIntroIndicator.apply {
+            setViewPager(binding.activityIntroVp)
+            createIndicators(2, 0)
+        }
+
+        // 뷰페이저 리스너
+        binding.activityIntroVp.registerOnPageChangeCallback( object : ViewPager2.OnPageChangeCallback() {
+            override fun onPageSelected(position: Int) {
+                when(position) {
+                    0 -> binding.activityIntroBtn.text = "다음"
+                    1 -> binding.activityIntroBtn.text = "시작하기"
+                }
+            }
+        })
+        // 버튼 클릭 리스너
+        binding.activityIntroBtn.setOnClickListener {
+            if(binding.activityIntroBtn.text == "다음") {
+                binding.activityIntroVp.currentItem = 1
+            } else {
+                startActivity(Intent(this, LoginActivity::class.java))
+            }
+        }
+    }
+    inner class IntroViewPagerAdapter(fragmentActivity: FragmentActivity) : FragmentStateAdapter(fragmentActivity) {
+        override fun getItemCount(): Int = 2
+
+        override fun createFragment(position: Int): Fragment {
+            Log.d("vpAdapter","$position")
+            return when(position) {
+                0 -> {
+                    binding.activityIntroBtn.text = "다음"
+                    IntroFirstFragment()
+                }
+                1 -> {
+                    binding.activityIntroBtn.text = "시작하기"
+                    IntroSecondFragment()
+                }
+                else -> IntroFirstFragment()
+            }
+        }
+
+    }
+}
--- Garamgaebi/app/src/main/java/com/softsquared/template/Garamgaebi/src/main/register/IntroSecondFragment.kt
+++ Garamgaebi/app/src/main/java/com/softsquared/template/Garamgaebi/src/main/register/IntroSecondFragment.kt
@@ -1,14 +1,15 @@
-package com.softsquared.template.Garamgaebi.src.main.home
+package com.softsquared.template.Garamgaebi.src.main.register
 
 import android.os.Bundle
 import android.view.View
 import com.softsquared.template.Garamgaebi.R
 import com.softsquared.template.Garamgaebi.config.BaseFragment
-import com.softsquared.template.Garamgaebi.databinding.FragmentHomeBinding
+import com.softsquared.template.Garamgaebi.databinding.FragmentIntroSecondBinding
 
-
-class HomeFragment : BaseFragment<FragmentHomeBinding>(FragmentHomeBinding::bind, R.layout.fragment_home) {
+class IntroSecondFragment : BaseFragment<FragmentIntroSecondBinding>(
+    FragmentIntroSecondBinding::bind, R.layout.fragment_intro_second){
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
     }
 }
