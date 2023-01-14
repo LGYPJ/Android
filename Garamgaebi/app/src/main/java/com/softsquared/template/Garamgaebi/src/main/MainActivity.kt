903529ba81506383a7fc133ea95f4238769693a2
CHAEWON LEE <chaewon1769@daum.net>
2023-01-15 01:52:01 +0900

    네트워킹 게임 일단 merge

--- Garamgaebi/app/src/main/java/com/softsquared/template/Garamgaebi/src/main/MainActivity.kt
+++ Garamgaebi/app/src/main/java/com/softsquared/template/Garamgaebi/src/main/MainActivity.kt
@@ -5,33 +5,14 @@ import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.softsquared.template.Garamgaebi.R
 import com.softsquared.template.Garamgaebi.config.BaseActivity
 import com.softsquared.template.Garamgaebi.databinding.ActivityMainBinding
-import com.softsquared.template.Garamgaebi.src.main.home.HomeFragment
-import com.softsquared.template.Garamgaebi.src.main.myPage.MyPageFragment
+
 
 class MainActivity : BaseActivity<ActivityMainBinding>(ActivityMainBinding::inflate) {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        supportFragmentManager.beginTransaction().replace(R.id.main_frm, HomeFragment()).commitAllowingStateLoss()
 
-        binding.mainBtmNav.run {
-            setOnItemSelectedListener { item ->
-                when (item.itemId) {
-                    R.id.menu_main_btm_nav_home -> {
-                        supportFragmentManager.beginTransaction()
-                            .replace(R.id.main_frm, HomeFragment())
-                            .commitAllowingStateLoss()
-                    }
-                    R.id.menu_main_btm_nav_my_page -> {
-                        supportFragmentManager.beginTransaction()
-                            .replace(R.id.main_frm, MyPageFragment())
-                            .commitAllowingStateLoss()
-                    }
-                }
-                true
-            }
-            selectedItemId = R.id.menu_main_btm_nav_home
         }
-    }
+
 }
