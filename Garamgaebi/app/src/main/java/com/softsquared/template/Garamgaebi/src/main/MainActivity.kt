8cf0b8506b381b28f8f2a0ea6ef57ccc87f9bfa6
Dongwon Shin <edvedv@naver.com>
2023-01-16 02:23:02 +0900

    Fix : 메인 액티비티 수정

--- Garamgaebi/app/src/main/java/com/softsquared/template/Garamgaebi/src/main/MainActivity.kt
+++ Garamgaebi/app/src/main/java/com/softsquared/template/Garamgaebi/src/main/MainActivity.kt
@@ -1,37 +1,45 @@
 package com.softsquared.template.Garamgaebi.src.main
 
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.softsquared.template.Garamgaebi.R
 import com.softsquared.template.Garamgaebi.config.BaseActivity
 import com.softsquared.template.Garamgaebi.databinding.ActivityMainBinding
 import com.softsquared.template.Garamgaebi.src.main.home.HomeFragment
 import com.softsquared.template.Garamgaebi.src.main.myPage.MyPageFragment
+import com.softsquared.template.Garamgaebi.src.profile.MyProfileActivity
 
 class MainActivity : BaseActivity<ActivityMainBinding>(ActivityMainBinding::inflate) {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        supportFragmentManager.beginTransaction().replace(R.id.main_frm, HomeFragment()).commitAllowingStateLoss()
-
-        binding.mainBtmNav.run {
-            setOnItemSelectedListener { item ->
-                when (item.itemId) {
-                    R.id.menu_main_btm_nav_home -> {
-                        supportFragmentManager.beginTransaction()
-                            .replace(R.id.main_frm, HomeFragment())
-                            .commitAllowingStateLoss()
-                    }
-                    R.id.menu_main_btm_nav_my_page -> {
-                        supportFragmentManager.beginTransaction()
-                            .replace(R.id.main_frm, MyPageFragment())
-                            .commitAllowingStateLoss()
-                    }
+        supportFragmentManager.beginTransaction().replace(R.id.activity_main_frm, HomeFragment()).commitAllowingStateLoss()
+        setBottomNavi()
+    }
+    //이벤트 리스너 역할. 하단 네비게이션 이벤트에 따라 화면을 리턴한다.
+    private fun setBottomNavi() {
+        binding.activityMainBottomNavi.setOnItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.activity_main_btm_nav_home -> {
+                    setFragment(HomeFragment())
+                    return@setOnItemSelectedListener true
                 }
-                true
+                //R.id.activity_main_btm_nav_gathering -> {
+                //    setFragment(GatheringFragment())
+                //    return@setOnItemSelectedListener true
+                //}
+                //R.id.activity_main_btm_nav_profile -> {
+                //    setFragment(MyProfileFragment())
+                //    return@setOnItemSelectedListener true
+                //}
+                else -> false
             }
-            selectedItemId = R.id.menu_main_btm_nav_home
         }
+    }
+
+    private fun setFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction().replace(R.id.activity_main_frm, fragment).commit()
     }
 }
