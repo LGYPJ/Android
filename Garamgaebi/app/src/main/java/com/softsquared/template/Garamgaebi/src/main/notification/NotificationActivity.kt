e05f0e54f79cbd0715c9e0cb5fd820ed014dd3c3
Dongwon Shin <edvedv@naver.com>
2023-01-16 21:50:10 +0900

    Fix: RecyclerView bug

--- Garamgaebi/app/src/main/java/com/softsquared/template/Garamgaebi/src/main/notification/NotificationActivity.kt
+++ Garamgaebi/app/src/main/java/com/softsquared/template/Garamgaebi/src/main/notification/NotificationActivity.kt
@@ -1,7 +1,7 @@
 package com.softsquared.template.Garamgaebi.src.main.notification
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.softsquared.template.Garamgaebi.R
 import com.softsquared.template.Garamgaebi.config.BaseActivity
 import com.softsquared.template.Garamgaebi.databinding.ActivityNotificationBinding
@@ -11,7 +11,7 @@ class NotificationActivity : BaseActivity<ActivityNotificationBinding>(ActivityN
         super.onCreate(savedInstanceState)
 
         val itemDataList : ArrayList<NotificationItemData> = arrayListOf()
-        for( i in 1..3) {
+        for( i : Int in 1..3) {
             itemDataList.add(NotificationItemData(R.drawable.ic_item_activity_notification_gathering, "모아보기","새로운 네트워킹이 오픈되었어요"))
             itemDataList.add(NotificationItemData(R.drawable.ic_item_activity_notification_complete, "신청완료","2차 네트워킹 신청이 완료되었어요"))
             itemDataList.add(NotificationItemData(R.drawable.ic_item_activity_notification_deadline, "마감임박","2차 세미나 마감이 임박했어요"))
@@ -19,6 +19,9 @@ class NotificationActivity : BaseActivity<ActivityNotificationBinding>(ActivityN
 
         val notificationRVAdapter = NotificationItemRVAdapter(itemDataList)
         binding.activityNotificationRv.adapter = notificationRVAdapter
-
+        binding.activityNotificationRv.layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
+        binding.activityNotificationBtnBack.setOnClickListener {
+            onBackPressed()
+        }
     }
 }
