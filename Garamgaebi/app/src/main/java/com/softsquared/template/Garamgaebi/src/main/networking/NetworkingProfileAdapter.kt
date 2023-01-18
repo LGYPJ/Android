d2a8795065997b6b60b2182911ef91bb7fbc1a8b
Lee Chaewon <chaewon1769@daum.net>
2023-01-18 12:56:45 +0900

    Feat: 네트워킹&게임 기능 추가

--- Garamgaebi/app/src/main/java/com/softsquared/template/Garamgaebi/src/main/networking/NetworkingProfileAdapter.kt
+++ Garamgaebi/app/src/main/java/com/softsquared/template/Garamgaebi/src/main/networking/NetworkingProfileAdapter.kt
@@ -3,11 +3,12 @@ package com.softsquared.template.Garamgaebi.src.main.networking
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
-import com.softsquared.template.Garamgaebi.databinding.ItemNetworkProfileBinding
+import com.softsquared.template.Garamgaebi.databinding.*
+import com.softsquared.template.Garamgaebi.src.seminar.*
 
-class NetworkingProfileAdapter(private val dataList: ArrayList<NetworkingProfile>): RecyclerView.Adapter<NetworkingProfileAdapter.ViewHolder>() {
+class NetworkingProfileAdapter(private val dataList: ArrayList<NetworkingProfile>): RecyclerView.Adapter<RecyclerView.ViewHolder>() {
 
-    inner class ViewHolder(private val binding: ItemNetworkProfileBinding):
+    inner class OriginViewHolder(private val binding: ItemNetworkProfileBinding):
             RecyclerView.ViewHolder(binding.root) {
                 fun bind(data: NetworkingProfile){
                     binding.itemProfileImg.setImageResource(data.img)
@@ -15,17 +16,64 @@ class NetworkingProfileAdapter(private val dataList: ArrayList<NetworkingProfile
                 }
             }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val binding = ItemNetworkProfileBinding.inflate(LayoutInflater.from(parent.context), parent, false)
-        return ViewHolder(binding)
+    inner class BlueViewHolder(private val binding: ItemNetworkProfileBlueBinding): RecyclerView.ViewHolder(binding.root) {
+        fun bind(data: NetworkingProfile){
+            binding.itemProfileImg.setImageResource(data.img)
+            binding.itemProfileNameTv.text = data.name
+        }
+    }
+
+    inner class GrayViewHolder(private val binding: ItemNetworkProfileGrayBinding): RecyclerView.ViewHolder(binding.root) {
+        fun bind(data: NetworkingProfile){
+            binding.itemProfileImg.setImageResource(data.img)
+            binding.itemProfileNameTv.text = data.name
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        return when(viewType){
+            multi_type1 -> {
+                val binding = ItemNetworkProfileBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+                OriginViewHolder(binding)
+            }
+            multi_type2 -> {
+                val binding = ItemNetworkProfileBlueBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+                BlueViewHolder(binding)
+            }
+            multi_type3 -> {
+                val binding = ItemNetworkProfileGrayBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+                GrayViewHolder(binding)
+            }
+            else -> {
+                val binding = ItemNetworkProfileBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+                OriginViewHolder(binding)
+            }
+        }
 
     }
 
-    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        holder.bind(dataList[position])
-    }
 
     override fun getItemCount(): Int {
         return dataList.size
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        when(dataList[position].type){
+            multi_type1 -> {
+                (holder as OriginViewHolder).bind(dataList[position])
+                holder.setIsRecyclable(false)
+            }
+            multi_type2 -> {
+                (holder as BlueViewHolder).bind(dataList[position])
+                holder.setIsRecyclable(false)
+            }
+            multi_type3 -> {
+                (holder as GrayViewHolder).bind(dataList[position])
+                holder.setIsRecyclable(false)
+            }
+        }
+    }
+    override fun getItemViewType(position: Int): Int {
+        return dataList[position].type
     }
 }
