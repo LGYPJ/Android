39f70c5e2e4449df462ff0391d179768671c9d3f
Dongwon Shin <edvedv@naver.com>
2023-01-19 17:13:26 +0900

    Fix : Merge conflict resolve

--- Garamgaebi/app/src/main/java/com/softsquared/template/Garamgaebi/src/main/networking/NetworkingProfile.kt
+++ Garamgaebi/app/src/main/java/com/softsquared/template/Garamgaebi/src/main/networking/NetworkingProfile.kt
@@ -2,7 +2,6 @@ package com.softsquared.template.Garamgaebi.src.main.networking
 
 data class NetworkingProfile(
     val img: Int,
-    val name : String
     val name : String,
     val type : Int
 )
