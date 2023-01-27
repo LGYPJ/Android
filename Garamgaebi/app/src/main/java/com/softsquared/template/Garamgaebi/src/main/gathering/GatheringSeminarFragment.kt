f993d3cbe1e48b6167badb00c67a7f9668195e6a
Lee Chaewon <chaewon1769@daum.net>
2023-01-27 21:25:09 +0900

    Feat: 툴바 추가, 세미나 프래그먼트로 임시 변경

--- Garamgaebi/app/src/main/java/com/softsquared/template/Garamgaebi/src/main/gathering/GatheringSeminarFragment.kt
+++ Garamgaebi/app/src/main/java/com/softsquared/template/Garamgaebi/src/main/gathering/GatheringSeminarFragment.kt
@@ -7,13 +7,18 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import com.softsquared.template.Garamgaebi.R
 import com.softsquared.template.Garamgaebi.config.BaseFragment
 import com.softsquared.template.Garamgaebi.databinding.FragmentGatheringSeminarBinding
+import com.softsquared.template.Garamgaebi.databinding.FragmentSeminarBinding
+import com.softsquared.template.Garamgaebi.src.main.ContainerActivity
 import com.softsquared.template.Garamgaebi.src.main.home.GatheringItemDecoration
 import com.softsquared.template.Garamgaebi.src.main.home.HomeSeminarItemData
 import com.softsquared.template.Garamgaebi.src.main.networking.NetworkingActivity
+import com.softsquared.template.Garamgaebi.src.seminar.SeminarFragment
 import com.softsquared.template.Garamgaebi.src.seminar.SeminarFreeActivity
 import com.softsquared.template.Garamgaebi.src.seminar.SeminarFreeApplyActivity
 
 class GatheringSeminarFragment : BaseFragment<FragmentGatheringSeminarBinding>(FragmentGatheringSeminarBinding::bind, R.layout.fragment_gathering_seminar){
+
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         var seminarDataList : ArrayList<GatheringSeminarItemData> = arrayListOf(
@@ -43,7 +48,13 @@ class GatheringSeminarFragment : BaseFragment<FragmentGatheringSeminarBinding>(F
         }
         seminarThisMonthAdapter.setOnItemClickListener(object : GatheringSeminarThisMonthRVAdapter.OnItemClickListener{
             override fun onClick(position: Int) {
-                //TODO("Not yet implemented")
+                //세미나 메인 프래그먼트로!
+                /*val intent = Intent(context, SeminarFragment::class.java)
+                val tmp = "gathering_seminar"
+                intent.putExtra("gathering_seminar",tmp )
+                startActivity(intent)*/
+
+
             }
         })
         seminarScheduledAdapter.setOnItemClickListener(object : GatheringSeminarScheduledRVAdapter.OnItemClickListener{
