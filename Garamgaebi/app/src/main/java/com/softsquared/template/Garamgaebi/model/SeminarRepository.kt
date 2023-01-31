58ef8617c413b3bb68c0f13f1841e40cb334fd7e
1109min <1109min@naver.com>
2023-01-31 23:38:54 +0900

    Feat: Packaging Models

--- Garamgaebi/app/src/main/java/com/softsquared/template/Garamgaebi/model/SeminarRepository.kt
+++ Garamgaebi/app/src/main/java/com/softsquared/template/Garamgaebi/model/SeminarRepository.kt
@@ -3,7 +3,7 @@ package com.softsquared.template.Garamgaebi.model
 import com.softsquared.template.Garamgaebi.config.GaramgaebiApplication
 
 class SeminarRepository {
-    private val seminarClient = GaramgaebiApplication.sRetrofit.create(SeminarInterface::class.java)
+    private val seminarClient = GaramgaebiApplication.sRetrofit.create(ApiInterface::class.java)
 
     suspend fun getSeminarsInfo(seminaridx : Int) = seminarClient.getSeminarsInfo(1)
 }
