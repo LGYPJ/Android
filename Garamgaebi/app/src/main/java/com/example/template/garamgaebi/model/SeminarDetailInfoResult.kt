7a6b14dadc7b98aa1a421e476c79854891cc49e6
Lee Chaewon <chaewon1769@daum.net>
2023-02-06 21:24:26 +0900

    Feat: 세미나 상세보기 데이터바인딩

---
+++ Garamgaebi/app/src/main/java/com/example/template/garamgaebi/model/SeminarDetailInfoResult.kt
@@ -0,0 +1,14 @@
+package com.example.template.garamgaebi.model
+
+import com.google.gson.annotations.SerializedName
+
+data class SeminarDetailInfoResult(
+    @SerializedName("programIdx") val programIdx: Int,
+    @SerializedName("title")val title: String,
+    @SerializedName("date")val date: String,
+    @SerializedName("location")val location: String,
+    @SerializedName("fee")val fee: Int,
+    @SerializedName("endDate")val endDate: String,
+    @SerializedName("programStatus")val programStatus: String,
+    @SerializedName("userButtonStatus")val userButtonStatus: String
+)
