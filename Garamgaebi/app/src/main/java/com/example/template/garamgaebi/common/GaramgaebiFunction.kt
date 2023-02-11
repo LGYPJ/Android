143275a3caf3d391e03daf011819640b20f5e53f
1109min <1109min@naver.com>
2023-02-11 16:35:05 +0900

    Feat: Implement career databinding

--- Garamgaebi/app/src/main/java/com/example/template/garamgaebi/common/GaramgaebiFunction.kt
+++ Garamgaebi/app/src/main/java/com/example/template/garamgaebi/common/GaramgaebiFunction.kt
@@ -1,10 +1,40 @@
 package com.example.template.garamgaebi.common
 
+import android.util.Log
+import android.view.View.OnFocusChangeListener
+import android.widget.EditText
+import androidx.databinding.BindingAdapter
+import com.example.template.garamgaebi.R
 import java.time.LocalDate
 import java.time.format.DateTimeFormatter
+
 
 class GaramgaebiFunction {
     fun getDay(date : String): String{
         return LocalDate.parse(date, DateTimeFormatter.ofPattern("yyyy-mm-dd")).toString()
     }
+
+
+    //editText focus Listener
+//    var onFocusChangeListener =
+//        OnFocusChangeListener { view, isFocused ->
+//            val origin = view.background
+//            if(isFocused){
+//                view.setBackgroundResource(R.drawable.basic_black_border_layout)
+//            }else{
+//                view.background = origin
+//            }
+//        }
+    interface OnFocusLostListener {
+        fun onFocusLost(view: EditText,boolean: Boolean)
+    }
+    fun logTest(view:EditText, check : Boolean,isValid : Boolean){
+        Log.d("focus_check_log",view.toString() + check.toString() + isValid.toString())
+    }
+
+
+
+
+
+
 }
--- Garamgaebi/app/src/main/java/com/example/template/garamgaebi/viewModel/SNSViewModel.kt
+++ Garamgaebi/app/src/main/java/com/example/template/garamgaebi/viewModel/SNSViewModel.kt
@@ -1,6 +1,7 @@
 package com.example.template.garamgaebi.viewModel
 
 import android.util.Log
+import android.widget.EditText
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
@@ -11,122 +12,47 @@ import kotlinx.coroutines.launch
 
 class SNSViewModel : ViewModel(){
     private val profileRepository = ProfileRepository()
+    var addressFirst : Boolean = false
+    var typeFirst: Boolean = false
 
-    lateinit var snsType : String
-    lateinit var snsAddress : String
+    val snsType = MutableLiveData<String>()
+    init { snsType.value = ""}
+
+    val snsTypeIsValid = MutableLiveData<Boolean>()
+    init { snsTypeIsValid.value = false}
+
+    val snsAddress = MutableLiveData<String>()
+    init { snsAddress.value = ""}
+
+    val snsAddressIsValid = MutableLiveData<Boolean>()
+    init { snsAddressIsValid.value = false}
 
 
-    //프로필 정보 조회
-    private val _profileInfo = MutableLiveData<ProfileDataResponse>()
-    val profileInfo : LiveData<ProfileDataResponse>
-        get() = _profileInfo
+    fun logTest(view:EditText, check : Boolean,isValid : Boolean){
+        Log.d("focus_check_log",view.toString() + check.toString() + isValid.toString())
+    }
+    fun setTrueAddress(){
+        addressFirst = true
+    }
+    fun setTrueType(){
+        typeFirst = true
+    }
 
-    private val _myContent = MutableLiveData<String>()
-    val myContent : LiveData<String>
-        get() = _myContent
-
-    fun getProfileInfo(memberIdx : Int) {
+    private val _add = MutableLiveData<AddSNSDataResponse>()
+    val add : LiveData<AddSNSDataResponse>
+        get() = _add
+    //SNS 추가
+    fun postSNSInfo() {
         viewModelScope.launch {
-            val response = profileRepository.getProfileInfo(memberIdx)
-            Log.d("present0", response.body().toString())
-
-            if (response.isSuccessful || response.body()?.result ?: null != null) {
-                Log.d("success", response.message())
-                _profileInfo.postValue(response.body())
+            val response = profileRepository.getCheckAddSNS(SNSData(1,snsAddress.value.toString()))
+            //Log.d("sns_add", response.body().toString())
+            if(response.isSuccessful){
+                _add.postValue(response.body())
+                Log.d("sns_add_success", response.body().toString())
             }
             else {
-                Log.d("error", response.message())
+                //response.body()?.message?.let { Log.d("error", it) }
             }
         }
     }
-
-    //SNS 정보 조회
-    private val _snsInfo = MutableLiveData<SNSDataResponse>()
-    val snsInfo : LiveData<SNSDataResponse>
-        get() = _snsInfo
-
-    private val _snsInfoArray = MutableLiveData<ArrayList<SNSData>>()
-    val snsInfoArray : LiveData<ArrayList<SNSData>>
-        get() = _snsInfoArray
-
-    fun getSNSInfo(memberIdx : Int) {
-        viewModelScope.launch {
-            val response = profileRepository.getSNSInfo(memberIdx)
-            Log.d("api_sns", response.body().toString())
-
-            if (response.isSuccessful) {
-                //_snsInfo.postValue(response.body())
-                _snsInfoArray.postValue(response.body()?.result as ArrayList<SNSData>?)
-            }
-            else {
-                Log.d("error", response.message())
-            }
-        }
-    }
-
-    //교육 정보 조회
-    private val _educationInfo = MutableLiveData<EducationDataResponse>()
-    val educationInfo : LiveData<EducationDataResponse>
-        get() = _educationInfo
-
-    private val _educationInfoArray = MutableLiveData<ArrayList<EducationData>>()
-    val educationInfoArray : LiveData<ArrayList<EducationData>>
-        get() = _educationInfoArray
-
-    fun getEducationInfo(memberIdx : Int) {
-        viewModelScope.launch {
-            val response = profileRepository.getEducationInfo(memberIdx)
-            Log.d("api_edu", response.body().toString())
-
-            if (response.isSuccessful) {
-                _educationInfoArray.postValue(response.body()?.result as ArrayList<EducationData>)
-            }
-            else {
-                Log.d("error", response.message())
-            }
-        }
-    }
-
-    //경력 정보 조회
-    private val _careerInfo = MutableLiveData<CareerDataResponse>()
-    val careerInfo : LiveData<CareerDataResponse>
-        get() = _careerInfo
-
-    private val _careerInfoArray = MutableLiveData<ArrayList<CareerData>>()
-    val careerInfoArray : LiveData<ArrayList<CareerData>>
-        get() = _careerInfoArray
-
-    fun getCareerInfo(memberIdx : Int) {
-        viewModelScope.launch {
-            val response = profileRepository.getCareerInfo(memberIdx)
-            Log.d("api_career", response.body().toString())
-
-            if (response.isSuccessful) {
-                _careerInfoArray.postValue(response.body()?.result as ArrayList<CareerData>)
-            }
-            else {
-                Log.d("error", response.message())
-            }
-        }
-    }
-
-//    //추천 프로필 정보 조회
-//    private val _recommendInfo = MutableLiveData<RecommendProfileDataResponse>()
-//    val recommendInfo : LiveData<RecommendProfileDataResponse>
-//        get() = _recommendInfo
-//
-//    fun getRecommendProfile(memberIdx : Int) {
-//        viewModelScope.launch {
-//            val response = profileRepository.getRecommendProfile()
-//            Log.d("present", response.body().toString())
-//
-//            if (response.isSuccessful) {
-//                _recommendInfo.postValue(response.body())
-//            }
-//            else {
-//                Log.d("error", response.message())
-//            }
-//        }
-//    }
-
 }
