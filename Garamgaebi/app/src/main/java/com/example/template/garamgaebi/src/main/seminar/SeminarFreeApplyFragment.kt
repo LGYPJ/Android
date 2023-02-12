b82e3a144b2392a5b2596532dcb8635050e575ce
Lee Chaewon <chaewon1769@daum.net>
2023-02-12 21:11:56 +0900

    Feat: 신청하기 페이지들 정규식 사용

--- Garamgaebi/app/src/main/java/com/example/template/garamgaebi/src/main/seminar/SeminarFreeApplyFragment.kt
+++ Garamgaebi/app/src/main/java/com/example/template/garamgaebi/src/main/seminar/SeminarFreeApplyFragment.kt
@@ -12,6 +12,7 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import com.example.template.garamgaebi.R
 import com.example.template.garamgaebi.common.BaseFragment
+import com.example.template.garamgaebi.common.GaramgaebiApplication
 import com.example.template.garamgaebi.databinding.FragmentSeminarFreeApplyBinding
 import com.example.template.garamgaebi.src.main.ContainerActivity
 import com.example.template.garamgaebi.viewModel.ApplyViewModel
@@ -53,6 +54,33 @@ class SeminarFreeApplyFragment: BaseFragment<FragmentSeminarFreeApplyBinding>(Fr
         }
 
         // et에 따라 오류메세지 생성 & drawable 변경 & 신청하기버튼 활성화
+        binding.activitySeminarFreeApplyNameTv.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+
+            }
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+
+                if(!isName()){
+                    binding.activitySeminarFreeApplyNotCorrectNameTv.visibility = View.VISIBLE
+                    binding.activitySeminarFreeApplyNameTv.setBackgroundResource(R.drawable.activity_seminar_apply_red_border)
+                }
+                else{
+                    binding.activitySeminarFreeApplyNotCorrectNameTv.visibility = View.GONE
+                    binding.activitySeminarFreeApplyNameTv.setBackgroundResource(R.drawable.activity_seminar_et_border_gray)
+                }
+                if(isButton()){
+                    binding.activitySeminarFreeApplyBtn.isEnabled = true
+                    binding.activitySeminarFreeApplyBtn.setBackgroundResource(R.drawable.btn_seminar_apply)
+                }
+                else {
+
+                    binding.activitySeminarFreeApplyBtn.isEnabled = false
+                }
+            }
+            override fun afterTextChanged(p0: Editable?) {
+            }
+        })
+
         binding.activitySeminarFreeApplyNicknameTv.addTextChangedListener(object : TextWatcher {
             override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
 
@@ -143,39 +171,53 @@ class SeminarFreeApplyFragment: BaseFragment<FragmentSeminarFreeApplyBinding>(Fr
 
     }
 
+    // 이름 형식 맞나
+    fun isName() : Boolean {
+        var returnValue = false
+        val name = binding.activitySeminarFreeApplyNameTv.text.toString()
+        val regex = "^[ㄱ-ㅎㅏ-ㅣ가-힣a-zA-Z]{1,20}$"
+        val p = Pattern.compile(regex)
+        val m = p.matcher(name)
+        if (m.matches()) {
+            returnValue = true
+        }
+        return returnValue;
+    }
+
     //전화번호 형식 맞나
     fun isPhoneNumberCheck() : Boolean {
         var returnValue = false
         val phone = binding.activitySeminarFreeApplyPhoneTv.text.toString()
-        val regex = "^\\s*(010|011|012|013|014|015|016|017|018|019)(-|\\)|\\s)*(\\d{3,4})(-|\\s)*(\\d{4})\\s*$";
-        val p = Pattern.compile(regex);
-        val m = p.matcher(phone);
+        val regex = "^[0,9]{11}$"
+        val p = Pattern.compile(regex)
+        val m = p.matcher(phone)
         if (m.matches()) {
-            returnValue = true;
+            returnValue = true
         }
-        return returnValue;
+        return returnValue
     }
     //닉네임 맞나
     fun isNickName(): Boolean{
         var returnValue = false
         val nickname = binding.activitySeminarFreeApplyNicknameTv.text.toString()
-        val regex = "cindy"
-        val p = regex.matches(nickname.toRegex())
-        if(p){
-            returnValue = true;
+        //나중에 회원가입할 때 닉네임 로컬에 저장해서 regax에 선언하기
+        val regex = GaramgaebiApplication.sSharedPreferences.getString("nickname", null)
+        val p = regex?.matches(nickname.toRegex())
+        if(p == true){
+            returnValue = true
         }
-        return returnValue;
+        return returnValue
     }
 
+    //버튼 활성화
     fun isButton():Boolean {
         var returnValue = false
-        if(isNickName()&&isPhoneNumberCheck()){
-            returnValue = true;
+        if(isNickName()&&isPhoneNumberCheck()&&isName()){
+            returnValue = true
         }
-        return returnValue;
+        return returnValue
 
     }
-
     //화면전환
     override fun onAttach(context: Context) {
         super.onAttach(context)
