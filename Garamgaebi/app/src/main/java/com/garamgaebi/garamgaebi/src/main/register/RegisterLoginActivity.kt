050ee4b1bc23a21852235f0d4d1b42e973c42fe0
Dongwon Shin <edvedv@naver.com>
2023-02-17 02:44:26 +0900

    Fix : kakao login

--- Garamgaebi/app/src/main/java/com/garamgaebi/garamgaebi/src/main/register/RegisterLoginActivity.kt
+++ Garamgaebi/app/src/main/java/com/garamgaebi/garamgaebi/src/main/register/RegisterLoginActivity.kt
@@ -4,7 +4,6 @@ import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
-import com.garamgaebi.garamgaebi.BuildConfig
 import com.garamgaebi.garamgaebi.common.BaseActivity
 import com.garamgaebi.garamgaebi.databinding.ActivityRegisterLoginBinding
 import com.kakao.sdk.auth.model.OAuthToken
