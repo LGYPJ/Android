0e563c65ea908ba7dac1c22276be01bee719ca9a
Lee Chaewon <chaewon1769@daum.net>
2023-02-17 13:50:35 +0900

    Feat : 신청하기 databinding으로 변경

--- Garamgaebi/app/src/main/java/com/garamgaebi/garamgaebi/src/main/networking/NetworkingFreeApplyFragment.kt
+++ Garamgaebi/app/src/main/java/com/garamgaebi/garamgaebi/src/main/networking/NetworkingFreeApplyFragment.kt
@@ -28,130 +28,34 @@ class NetworkingFreeApplyFragment: BaseFragment<FragmentNetworkingFreeApplyBindi
         super.onViewCreated(view, savedInstanceState)
         //데이터바인딩
         binding.setVariable(BR.item, viewModel)
-        binding.activityNetworkFreeApplyBtn.isEnabled = false
+        binding.item = viewModel
+        binding.lifecycleOwner = this
 
-        // et selected 여부에 따라 drawable 결정
-        binding.activityNetworkFreeApplyNameTv.onFocusChangeListener = View.OnFocusChangeListener { view, hasFocus ->
-            if (hasFocus) {
-                view.setBackgroundResource(R.drawable.activity_seminar_et_border_gray)
-            } else {
-                view.setBackgroundResource(R.drawable.et_seminat_apply)
-            }
-        }
-        binding.activityNetworkFreeApplyNicknameTv.onFocusChangeListener = View.OnFocusChangeListener { view, hasFocus ->
-            if (hasFocus) {
-                view.setBackgroundResource(R.drawable.activity_seminar_et_border_gray)
-            } else {
-                view.setBackgroundResource(R.drawable.et_seminat_apply)
-
-            }
-        }
-        binding.activityNetworkFreeApplyPhoneTv.onFocusChangeListener = View.OnFocusChangeListener { view, hasFocus ->
-            if (hasFocus) {
-                view.setBackgroundResource(R.drawable.activity_seminar_et_border_gray)
-            } else {
-                view.setBackgroundResource(R.drawable.et_seminat_apply)
-            }
-        }
-
-        // et에 따라 tv 변경 & drawable 변경 & 신청하기버튼 활성화
-        binding.activityNetworkFreeApplyNicknameTv.addTextChangedListener(object : TextWatcher {
-            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
-
-            }
-            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
-
-                if(!isName()){
-                    binding.activityNetworkFreeApplyNotCorrectNicknameTv.visibility = View.VISIBLE
-                    binding.activityNetworkFreeApplyNicknameTv.setBackgroundResource(R.drawable.activity_seminar_apply_red_border)
-                }
-                else{
-                    binding.activityNetworkFreeApplyNotCorrectNicknameTv.visibility = View.GONE
-                    binding.activityNetworkFreeApplyNicknameTv.setBackgroundResource(R.drawable.activity_seminar_et_border_gray)
-                }
-                if(isButton()){
-
-                    binding.activityNetworkFreeApplyBtn.isEnabled = true
-                    binding.activityNetworkFreeApplyBtn.setBackgroundResource(R.drawable.btn_seminar_apply)
-                }
-                else {
-
-                    binding.activityNetworkFreeApplyBtn.isEnabled = false
-                }
-            }
-            override fun afterTextChanged(p0: Editable?) {
-            }
+        viewModel.inputName.observe(viewLifecycleOwner, Observer{
+            binding.item = viewModel
+            viewModel.nameIsValid.value = it.isNotEmpty() && isName(it)
         })
 
-        binding.activityNetworkFreeApplyNameTv.addTextChangedListener(object : TextWatcher {
-            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
-
-            }
-            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
-
-                if(!isNickName()){
-                    binding.activityNetworkFreeApplyNotCorrectNameTv.visibility = View.VISIBLE
-                    binding.activityNetworkFreeApplyNameTv.setBackgroundResource(R.drawable.activity_seminar_apply_red_border)
-                }
-                else{
-                    binding.activityNetworkFreeApplyNotCorrectNameTv.visibility = View.GONE
-                    binding.activityNetworkFreeApplyNameTv.setBackgroundResource(R.drawable.activity_seminar_et_border_gray)
-                }
-                if(isButton()){
-
-                    binding.activityNetworkFreeApplyBtn.isEnabled = true
-                    binding.activityNetworkFreeApplyBtn.setBackgroundResource(R.drawable.btn_seminar_apply)
-                }
-                else {
-
-                    binding.activityNetworkFreeApplyBtn.isEnabled = false
-                }
-            }
-            override fun afterTextChanged(p0: Editable?) {
-            }
+        viewModel.inputNickName.observe(viewLifecycleOwner, Observer{
+            binding.item = viewModel
+            viewModel.nicknameIsValid.value = it.isNotEmpty() && isNickName(it)
         })
 
-        binding.activityNetworkFreeApplyPhoneTv.addTextChangedListener(object : TextWatcher {
-            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
-
-            }
-            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
-                if(!isPhoneNumberCheck()){
-                    binding.activityNetworkFreeApplyNotCorrectPhoneTv.visibility = View.VISIBLE
-                    binding.activityNetworkFreeApplyPhoneTv.setBackgroundResource(R.drawable.activity_seminar_apply_red_border)
-                }
-                else{
-                    binding.activityNetworkFreeApplyNotCorrectPhoneTv.visibility = View.GONE
-                    binding.activityNetworkFreeApplyPhoneTv.setBackgroundResource(R.drawable.activity_seminar_et_border_gray)
-                }
-                if(isButton()){
-
-                    binding.activityNetworkFreeApplyBtn.isEnabled = true
-                    binding.activityNetworkFreeApplyBtn.setBackgroundResource(R.drawable.btn_seminar_apply)
-                }
-                else {
-
-                    binding.activityNetworkFreeApplyBtn.isEnabled = false
-                }
-            }
-            override fun afterTextChanged(p0: Editable?) {
-            }
-
+        viewModel.inputPhone.observe(viewLifecycleOwner, Observer{
+            binding.item = viewModel
+            viewModel.phoneIsValid.value = it.isNotEmpty() && isPhoneNumberCheck(it)
         })
+
+        with(viewModel){
+            nameState.value = getString(R.string.apply_not_name)
+            nicknameState.value = getString(R.string.apply_not_nickname)
+            phoneState.value = getString(R.string.apply_not_phone)
+        }
+
+
 
         //신청하기 버튼 누르면 버튼 바뀌는 값 전달
         binding.activityNetworkFreeApplyBtn.setOnClickListener {
-            /*val bundle = Bundle()
-            bundle.putBoolean("networking",true)
-            val networkingFragment = NetworkingFragment()
-            networkingFragment.arguments = bundle
-            val transaction = parentFragmentManager.beginTransaction()
-            transaction.replace(R.id.activity_seminar_frame, networkingFragment).commit()*/
-            /*val name = binding.activityNetworkFreeApplyNameTv.text.toString()
-            val nickname = binding.activityNetworkFreeApplyNicknameTv.text.toString()
-            val phone = binding.activityNetworkFreeApplyPhoneTv.text.toString()*/
-            //신청 등록 api
-            //viewModel.postEnroll(EnrollRequest(0,0,name,nickname,phone))
             viewModel.postEnroll()
             viewModel.enroll.observe(viewLifecycleOwner, Observer {
                 binding.item = viewModel
@@ -178,9 +82,8 @@ class NetworkingFreeApplyFragment: BaseFragment<FragmentNetworkingFreeApplyBindi
     }
 
     // 이름 형식 맞나
-    fun isName() : Boolean {
+    fun isName(name : String) : Boolean {
         var returnValue = false
-        val name = binding.activityNetworkFreeApplyNameTv.text.toString()
         val regex = "^[ㄱ-ㅎㅏ-ㅣ가-힣a-zA-Z]{1,20}$"
         val p = Pattern.compile(regex)
         val m = p.matcher(name)
@@ -191,9 +94,8 @@ class NetworkingFreeApplyFragment: BaseFragment<FragmentNetworkingFreeApplyBindi
     }
 
     //전화번호 형식 맞나
-    fun isPhoneNumberCheck() : Boolean {
+    fun isPhoneNumberCheck( phone : String) : Boolean {
         var returnValue = false
-        val phone = binding.activityNetworkFreeApplyPhoneTv.text.toString()
         val regex = "^[0-9]{11}$"
         val p = Pattern.compile(regex)
         val m = p.matcher(phone)
@@ -203,25 +105,16 @@ class NetworkingFreeApplyFragment: BaseFragment<FragmentNetworkingFreeApplyBindi
         return returnValue
     }
     //닉네임 맞나
-    fun isNickName(): Boolean{
+    fun isNickName(nickname : String): Boolean{
         var returnValue = false
-        val nickname = binding.activityNetworkFreeApplyNicknameTv.text.toString()
         //나중에 회원가입할 때 닉네임 로컬에 저장해서 regax에 선언하기
         val regex = "zzangu"
-        val p = regex?.matches(nickname.toRegex())
+        val p = regex.matches(nickname.toRegex())
         if(p == true){
             returnValue = true
         }
         return returnValue
     }
 
-    //버튼 활성화
-    fun isButton():Boolean {
-        var returnValue = false
-        if(isNickName()&&isPhoneNumberCheck()&&isName()){
-            returnValue = true
-        }
-        return returnValue
 
-    }
 }
